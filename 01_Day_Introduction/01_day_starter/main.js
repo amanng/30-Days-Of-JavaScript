dc042046d8893129cf15d7630a8d20b7b9b8ad3d
shiferaw nigatu <83336957+amanng@users.noreply.github.com>
2026-01-24 17:38:22 +0300

    Day 01 Javascript

---
+++ 01_Day_Introduction/01_day_starter/main.js
@@ -0,0 +1,18 @@
+console.log(firstName, lastName, country, city, age, isMarried);
+console.log(gravity, boilingPoint, pi);
+console.log(name, job, live);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_Day_Introduction/01_day_starter/variable.js
@@ -0,0 +1,13 @@
+// Declaring variables to store personal information
+let firstName = "Shiferaw";
+let lastName = "Nigatu";
+let country = "Ethiopia";
+let city = "Addis Ababa";
+let age = 25;
+let isMarried = false;
+//Declaring variables with numeric values
+const gravity = 9.81;
+const boilingPoint = 100;
+const pi = 3.14;
+//Variables can aalso be declared in one line
+let name = "Shiferaw", job = "Developer", live = "Ethiopia";
