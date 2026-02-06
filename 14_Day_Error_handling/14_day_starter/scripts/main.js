23a27bb471d6da566356fc67e17829857f1fdc9e
shiferaw nigatu <83336957+amanng@users.noreply.github.com>
2026-02-06 09:04:08 +0300

    Day 14 Javascript

---
+++ 14_Day_Error_handling/14_day_starter/scripts/main.js
@@ -0,0 +1,21 @@
+try {
+    let firstname = "aman"
+    let fullName=firstname + ' ' + lastName
+}
+catch (err) {
+    console.log(err)
+}
+const throwErrorExampleFun = () => {
+  let message
+  let x = prompt('Enter a number: ')
+  try {
+    if (x == '') throw 'empty'
+    if (isNaN(x)) throw 'not a number'
+    x = Number(x)
+    if (x < 5) throw 'too low'
+    if (x > 10) throw 'too high'
+  } catch (err) {
+    console.log(err)
+  }
+}
+throwErrorExampleFun()
